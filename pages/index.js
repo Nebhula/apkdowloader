f046257d0ec84d358df5330fe814eebb0dbbaad0
Nebhula <josema.mera37@gmail.com>
2025-08-17 01:51:41 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -278,10 +278,9 @@ export default function Home() {
     />
   </div>
 
-{/* Bloque de imagen de Blogger con función atrás */}
-  <div style={{ width: '100%' }}>
-    <a href="https://avianuncios.com/usuario-pro" style={{ display: 'block', width: '100%' }}>
-      <div
+{/* Bloque de imagen de Blogger con link */}
+<div style={{ width: '100%' }}>
+  <a href="https://avianuncios.com/usuario-pro" style={{ display: 'block', width: '100%' }}>
     <img
       src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjg7LPnGp9yWlgV7MZ8oeHiiWveZKdYqZibqGB0di7OTMcdMA7yz1MDBPDjY8MQe1qp2cEG4o3H6yjgazhDKwoLDJevRhDiRFvJGMpSvSj0e76236KdzCm2MRLcu4p9Bs4Okpzk5bW9RdJf6ah4M14kUx1pMVZci9KZ_nfMTPm0CsEF3LztsIDOCAe9OxE/s1600/Copia%20de%20Copia%20de%20king%20%2822%29.png"
       alt="Imagen Blogger"
@@ -293,7 +292,9 @@ export default function Home() {
         display: 'block',
       }}
     />
-  </div>
+  </a>
+</div>
+
 
 
                                 
